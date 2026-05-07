21e2b49f3cb90304bf12fb1ab6df835a95a41540
rjdshureybeyhuigebbhujbyubbykhuerhyu <andrew.heesacker@student.csd509j.net>
2026-05-07 16:58:04 -0700

    Now generates a valid board. Eventually. Next up, matching.

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
 namespace SpriteKind {
     export const Cursor = SpriteKind.create()
 }
+function areThereMatchedGems () {
+    for (let i = 0; i <= 7; i++) {
+        for (let j = 0; j <= 7; j++) {
+            if (j < 6) {
+                if (The_grid[i][j] == The_grid[i][j + 1]) {
+                    if (The_grid[i][j] == The_grid[i][j + 2]) {
+                        return true
+                    }
+                }
+            }
+            if (i < 6) {
+                if (The_grid[i][j] == The_grid[i + 1][j]) {
+                    if (The_grid[i][j] == The_grid[i + 2][j]) {
+                        return true
+                    }
+                }
+            }
+        }
+    }
+    console.log("" + The_grid[0][0] + The_grid[0][1] + The_grid[0][2] + The_grid[0][3])
+    return false
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     changecursorpos(0, -1)
 })
@@ -9,25 +31,32 @@ function userSpin () {
     updateBoard()
 }
 function Reset_board () {
-    gemlist = [
-    assets.tile`nothing`,
-    assets.tile`blue`,
-    assets.tile`red`,
-    assets.tile`yellow`,
-    assets.tile`orang`,
-    assets.tile`whit`,
-    assets.tile`ourple`,
-    assets.tile`geen`
-    ]
-    for (let i = 0; i <= 7; i++) {
-        if (The_grid.length - 1 != i) {
-            The_grid.push([randint(1, 7)])
-        }
-        for (let j = 0; j <= 7; j++) {
-            if (The_grid[i].length - 1 != j) {
-                The_grid[i].push(randint(1, 7))
+    latestmatchedcall = true
+    while (latestmatchedcall) {
+        tries += 1
+        gemlist = [
+        assets.tile`nothing`,
+        assets.tile`blue`,
+        assets.tile`red`,
+        assets.tile`yellow`,
+        assets.tile`orang`,
+        assets.tile`whit`,
+        assets.tile`ourple`,
+        assets.tile`geen`
+        ]
+        The_grid = [[randint(1, 7)]]
+        for (let i = 0; i <= 7; i++) {
+            if (The_grid.length - 1 != i) {
+                The_grid.push([randint(1, 7)])
+            }
+            for (let j = 0; j <= 7; j++) {
+                if (The_grid[i].length - 1 != j) {
+                    The_grid[i].push(randint(1, 3))
+                }
             }
         }
+        latestmatchedcall = areThereMatchedGems()
+        console.log(tries)
     }
     updateBoard()
 }
@@ -35,7 +64,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     userSpin()
 })
 function changecursorpos (col: number, row: number) {
-    truecursorpos = [Math.constrain(truecursorpos[0] + col, 0, 7), Math.constrain(truecursorpos[1] + row, 0, 7)]
+    truecursorpos = [Math.constrain(truecursorpos[0] + col, 0, 6), Math.constrain(truecursorpos[1] + row, 0, 6)]
     placeCursorOnGrid(truecursorpos[0], truecursorpos[1])
 }
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -73,6 +102,8 @@ function Rotate (col: number, row: number) {
 }
 let cba: number[] = []
 let gemlist: Image[] = []
+let tries = 0
+let latestmatchedcall = false
 let truecursorpos: number[] = []
 let mySprite: Sprite = null
 let The_grid: number[][] = []
