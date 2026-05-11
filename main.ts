4008f2c1523758581873cb811d0005871da2e107
rjdshureybeyhuigebbhujbyubbykhuerhyu <andrew.heesacker@student.csd509j.net>
2026-05-11 10:55:37 -0700

    gems can now be cleared

--- main.ts
+++ main.ts
@@ -1,26 +1,29 @@
 namespace SpriteKind {
     export const Cursor = SpriteKind.create()
 }
-function areThereMatchedGems () {
-    for (let i = 0; i <= 7; i++) {
-        for (let j = 0; j <= 7; j++) {
-            if (j < 6) {
-                if (The_grid[i][j] == The_grid[i][j + 1]) {
-                    if (The_grid[i][j] == The_grid[i][j + 2]) {
-                        return true
+function areThereMatchedGems (array: number[][]) {
+    if (array[0][0] != 99) {
+        for (let i = 0; i <= 7; i++) {
+            for (let j = 0; j <= 7; j++) {
+                if (array[i][j] != 0) {
+                    if (j < 6) {
+                        if (array[i][j] == array[i][j + 1]) {
+                            if (array[i][j] == array[i][j + 2]) {
+                                return true
+                            }
+                        }
                     }
-                }
-            }
-            if (i < 6) {
-                if (The_grid[i][j] == The_grid[i + 1][j]) {
-                    if (The_grid[i][j] == The_grid[i + 2][j]) {
-                        return true
+                    if (i < 6) {
+                        if (array[i][j] == array[i + 1][j]) {
+                            if (array[i][j] == array[i + 2][j]) {
+                                return true
+                            }
+                        }
                     }
                 }
             }
         }
     }
-    console.log("" + The_grid[0][0] + The_grid[0][1] + The_grid[0][2] + The_grid[0][3])
     return false
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -28,7 +31,32 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 function userSpin () {
     The_grid = Rotate(truecursorpos[0], truecursorpos[1])
+    matchloc = butLikeWhere()
+    if (matchloc[0] != -1) {
+        while (true) {
+            // very inefficient. fix later?
+            matchloc = butLikeWhere()
+            if (matchloc[0] == -1) {
+                break;
+            } else {
+                clearGems(matchloc)
+            }
+            break;
+        }
+    } else {
+    	
+    }
     updateBoard()
+}
+function isAMovePossible () {
+    for (let k = 0; k <= 6; k++) {
+        for (let l = 0; l <= 6; l++) {
+            if (areThereMatchedGems(Rotate(k, l))) {
+                return true
+            }
+        }
+    }
+    return false
 }
 function Reset_board () {
     latestmatchedcall = true
@@ -45,17 +73,21 @@ function Reset_board () {
         assets.tile`geen`
         ]
         The_grid = [[randint(1, 7)]]
-        for (let k = 0; k <= 7; k++) {
-            if (The_grid.length - 1 != k) {
+        for (let m = 0; m <= 7; m++) {
+            if (The_grid.length - 1 != m) {
                 The_grid.push([randint(1, 7)])
             }
-            for (let l = 0; l <= 7; l++) {
-                if (The_grid[k].length - 1 != l) {
-                    The_grid[k].push(randint(1, 7))
+            for (let n = 0; n <= 7; n++) {
+                if (The_grid[m].length - 1 != n) {
+                    The_grid[m].push(randint(1, 7))
                 }
             }
         }
-        latestmatchedcall = areThereMatchedGems()
+        latestmatchedcall = areThereMatchedGems(The_grid)
+        if (!(latestmatchedcall)) {
+            latestmatchedcall = !(isAMovePossible())
+            console.log("wow it prevented an impossible board")
+        }
         console.log(tries)
     }
     updateBoard()
@@ -78,11 +110,11 @@ function duplicate2DList (array: number[][]) {
         } else {
             clone.push([])
         }
-        for (let j = 0; j <= array[index].length - 1; j++) {
+        for (let o = 0; o <= array[index].length - 1; o++) {
             if (clone[index].length - 1 < array[index].length - 1) {
-                clone[index].push(array[index][j])
+                clone[index].push(array[index][o])
             } else {
-                clone[index][j] = array[index][j]
+                clone[index][o] = array[index][o]
             }
         }
     }
@@ -94,15 +126,44 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     changecursorpos(0, 1)
 })
+function clearGems (identifiedMatch: number[]) {
+    lengthofmatch = 3
+    if (identifiedMatch[2] == 0) {
+        while (lengthofmatch + identifiedMatch[1] < 8) {
+            if (The_grid[identifiedMatch[0]][identifiedMatch[1]] == The_grid[identifiedMatch[0]][lengthofmatch + identifiedMatch[1]]) {
+                lengthofmatch += 1
+                console.log("+1")
+            } else {
+                break;
+            }
+        }
+    } else {
+        while (lengthofmatch + identifiedMatch[0] < 8) {
+            if (The_grid[identifiedMatch[0]][identifiedMatch[1]] == The_grid[identifiedMatch[0]][lengthofmatch + identifiedMatch[1]]) {
+                lengthofmatch += 1
+                console.log("+1")
+            } else {
+                break;
+            }
+        }
+    }
+    if (identifiedMatch[2] == 0) {
+        for (let index2 = 0; index2 <= lengthofmatch - 1; index2++) {
+            The_grid[identifiedMatch[0]][index2 + identifiedMatch[1]] = 0
+        }
+    } else {
+    	
+    }
+}
 function placeCursorOnGrid (col: number, row: number) {
     tiles.placeOnTile(mySprite, tiles.getTileLocation(col + 3, row))
     mySprite.x += 8
     mySprite.y += 8
 }
 function updateBoard () {
-    for (let m = 0; m <= 7; m++) {
-        for (let n = 0; n <= 7; n++) {
-            tiles.setTileAt(tiles.getTileLocation(n + 3, m), gemlist[The_grid[m][n]])
+    for (let p = 0; p <= 7; p++) {
+        for (let q = 0; q <= 7; q++) {
+            tiles.setTileAt(tiles.getTileLocation(q + 3, p), gemlist[The_grid[p][q]])
         }
     }
 }
@@ -119,12 +180,39 @@ function Rotate (col: number, row: number) {
     dupedgrid[row + 1][col] = cba[3]
     return dupedgrid
 }
+function butLikeWhere () {
+    if (The_grid[0][0] != 99) {
+        for (let r = 0; r <= 7; r++) {
+            for (let s = 0; s <= 7; s++) {
+                if (The_grid[r][s] != 0) {
+                    if (s < 6) {
+                        if (The_grid[r][s] == The_grid[r][s + 1]) {
+                            if (The_grid[r][s] == The_grid[r][s + 2]) {
+                                return [r, s, 0]
+                            }
+                        }
+                    }
+                    if (r < 6) {
+                        if (The_grid[r][s] == The_grid[r + 1][s]) {
+                            if (The_grid[r][s] == The_grid[r + 2][s]) {
+                                return [r, s, 1]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return [-1, -1, -1]
+}
 let cba: number[] = []
 let dupedgrid: number[][] = []
+let lengthofmatch = 0
 let clone: number[][] = []
 let gemlist: Image[] = []
 let tries = 0
 let latestmatchedcall = false
+let matchloc: number[] = []
 let truecursorpos: number[] = []
 let mySprite: Sprite = null
 let The_grid: number[][] = []
